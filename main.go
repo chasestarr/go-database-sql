8136f52bd728f3e1597a554f80b814f54f1d6a81
Chase Starr <chasestarr@gmail.com>
2017-01-10 21:04:26 -0800

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "dbname=pqgotest sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
